238c8709d04baaac024f167a398380b5815f30bf
tamago0224 <bigtreesasuke200733@gmail.com>
2017-06-12 23:04:14 +0900

    add img_show.py

---
+++ ch01/img_show.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#
+#Created 2017-06-12
+#
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+img = imread("/Users/ookikazuki/Pictures/壁紙/1080999.png")
+plt.imshow(img)
+plt.show()
