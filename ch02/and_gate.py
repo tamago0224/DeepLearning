65810c2a2cee7fbc69d0fda9883d4c5b4b9eeaac
tamago0224 <bigtreesasuke200733@gmail.com>
2017-06-13 10:51:36 +0900

    add and_gate.py

---
+++ ch02/and_gate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#
+#Created 2017-06-13
+#
+def AND(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    tmp = x1 * w1 + x2 * w2
+    if tmp > theta:
+        return 1
+    elif tmp <= theta:
+        return 0
+
+if __name__ == '__main__':
+    for xs in [(0,0), (1,0), (0,1), (1,1)]:
+        y = AND(xs[0], xs[1])
+        print(str(xs) + "->" + str(y))
